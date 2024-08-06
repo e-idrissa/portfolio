1eb260d731a15008dbbc3b04548813df8338a634
e-idrissa <id.hemedy98@gmail.com>
2024-08-07 01:00:38 +0200

    hero

--- components/custom/hero.tsx
+++ components/custom/hero.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link"
+import MagicButton from "../ui/magic-button"
 import { Spotlight } from "../ui/spotlight"
 import { TextGenerateEffect } from "../ui/text-generate"
+import { Navigation } from "lucide-react"
 
 const Hero = () => {
   return (
@@ -16,12 +19,19 @@ const Hero = () => {
         <div className="max-w-[89vw] relative md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase text-xs tracking-widest text-center text-blue-100 maw-w-80">Dynamic Web Magic with Next.js</h2>
           <TextGenerateEffect
-            className="text-center text-[40px] md:text-lg lg:text-3xl"
-            words="Transform Concepts into Seamless Experiences"
+            className="text-center text-[40px] md:text-2xl lg:text-5xl"
+            words="Transform Concepts into Seamless User Experiences"
           />
           <p className="text-center md:tracking-wider text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Eddy-Idrissa, a Next.js developper.
           </p>
+          <Link href="/about">
+            <MagicButton
+              title="Show my work"
+              icon={<Navigation size={16} fill="white"/>}
+              position="right"
+            />
+          </Link>
         </div>
       </div>
     </div>
---
+++ components/ui/magic-button.tsx
@@ -0,0 +1,22 @@
+type Props = {
+    title: string
+    icon: React.ReactNode
+    position: string
+    classNames?: string
+    onClick?: () => void
+}
+
+const MagicButton = ({ title, classNames, icon: Icon, position, onClick }: Props) => {
+  return (
+    <button className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 mt-10">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-5 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${classNames}`}>
+        {position === "left" && Icon}
+        {title}
+        {position === "right" && Icon}
+      </span>
+    </button>
+  );
+};
+
+export default MagicButton;
