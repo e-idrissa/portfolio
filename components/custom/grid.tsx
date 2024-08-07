dacf373b216b291689c954d4963dc68fe36b12ca
e-idrissa <id.hemedy98@gmail.com>
2024-08-07 20:57:35 +0200

    bento grid done

--- components/custom/grid.tsx
+++ components/custom/grid.tsx
@@ -4,17 +4,17 @@ import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>
+      <BentoGrid className="z-10">
       {gridItems.map(({id, className, title, description, img, imgClassName, titleClassName, spareImg}) => (
         <BentoGridItem
           key={id}
           id={id}
           title={title}
+          titleClassName={titleClassName}
           description={description}
           className={className}
           img={img}
           imgClassName={imgClassName}
-          titleClassName={titleClassName}
           spareImg={spareImg}
         />
       ))}
--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./gradient-animation-effect";
 
 export const BentoGrid = ({
   className,
@@ -23,53 +24,61 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  id,
+  titleClassName,
   img,
   imgClassName,
-  titleClassName,
   spareImg,
+  id,
 }: {
   className?: string;
-  id: number;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  img?: string;
-  imgClassName?: string;
-  titleClassName?: string;
-  spareImg?: string;
+  titleClassName?: string
+  img?: string
+  imgClassName?: string
+  spareImg?: string,
+  id?: number
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "relative row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
+        //   you can generate the color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && "flex items-center h-full"`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="h-full w-full absolute">
           {img && (
-            <img src={img} alt={img} className={cn(imgClassName, "object-cover object-center")} />
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+            />
           )}
         </div>
         <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}>
-        {spareImg && (
-            <img src={spareImg} alt={spareImg} className={`object-cover object-center w-full h-full`} />
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
           )}
         </div>
-      </div>
-      
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-50 font-bold"/>
+          </BackgroundGradientAnimation>
+        )}
+        <div className={cn(titleClassName, "group-hover/bento:-translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10")}>
+          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 uppercase">{description}</div>
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">{title}</div>
         </div>
       </div>
     </div>
